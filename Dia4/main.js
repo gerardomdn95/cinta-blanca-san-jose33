9f9afbfde0565fac2943b82964f0c03111b3f84a
gerardomdn95 <gerardo@devf.mx>
2019-08-26 09:14:13 -0600

    add Dia4

---
+++ Dia4/main.js
@@ -0,0 +1,37 @@
+// // alert("¡Funciona!");
+
+// // Este es un valor numérico
+// let numero = 5;
+
+// // Este es un valor de tipo string
+// let nombre = "Gerardo";
+
+// // Este es un valor booleano
+// let llueve = false;
+
+// // Operación
+// let x = 5;
+// let y = 3;
+
+// let suma = x + y;
+// let resta = x - y;
+// let mult = x * y;
+// let div = x / y;
+
+// console.log(numero);
+// console.log(nombre);
+// console.log(llueve);
+// console.log(suma);
+// console.log(resta);
+// console.log(mult);
+// console.log(div);
+
+let edad = 17;
+
+let numero = 9;
+
+if (edad >= 18) {
+    console.log("Eres mayor de edad");
+} else {
+    console.log("Eres menor de edad");
+}
