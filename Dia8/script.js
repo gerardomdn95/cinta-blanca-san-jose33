afa02befb0f895744bfbdcd125ff517c7cebf406
gerardomdn95 <gerardo@devf.mx>
2019-08-30 21:16:23 -0600

    functions

---
+++ Dia8/script.js
@@ -0,0 +1,42 @@
+// console.log("Funciona");
+
+// function imprimeNombre() {
+//     console.log("Gerardo");
+// }
+
+// imprimeNombre();
+
+// function convertir() {
+//     let c = prompt("Ingresa temperatura en Celsius");
+//     let f = (1.8 * c) + 32;
+//     console.log("La temperatura convertida es: " + f);
+// }
+
+// convertir();
+
+// function determinar(letra) {
+//     console.log(letra);
+//     if(letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u") {
+//         console.log("Es una vocal");
+//     } else {
+//         console.log("Es consonante");
+//     }
+// }
+
+// determinar("a");
+// determinar("b");
+// determinar("c");
+// determinar("d");
+// determinar("e");
+
+// let entrada = prompt("Ingresa letra");
+// determinar(entrada);
+
+// function suma(a, b) {
+//     let suma = a + b;
+//     console.log(suma);
+// }
+
+// suma(5, 3);
+// suma(3, 4);
+// suma(20000, 8382);
